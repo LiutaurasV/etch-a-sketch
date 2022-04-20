399d1df5f467ca8556e4ec85a184887e4f79c5cd
Liutauras <v.liutauras5@gmail.com>
2022-04-20 10:26:00 +0300

    Add a createGrid() function
    createGrid() creates a grid of n x n divs and adds it to the DOM

---
+++ script.js
@@ -0,0 +1,19 @@
+function createGrid(n) {
+    const grid = document.createElement('div')
+    grid.classList.add('grid')
+
+    for (let i = 1; i <= n; i++) {
+        let line = document.createElement('div');
+        line.classList.add('line');
+        grid.appendChild(line);
+
+        for (let j = 1; j <= n; j++) {
+            let block = document.createElement('div');
+            block.classList.add('block');
+            line.appendChild(block);
+        }
+    }
+    document.body.appendChild(grid)
+}
+
+createGrid(16);
