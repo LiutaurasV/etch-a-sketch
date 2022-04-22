32fcf9bb42a48139523e9cf616c8f8a44c4891b2
Liutauras <v.liutauras5@gmail.com>
2022-04-22 21:54:58 +0300

    Add a realistic mode
    The drawing process can now happen in three modes: regular, realistic and rainbow

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const slider = document.querySelector('#slider');
 const sliderInfo = document.querySelector(".sliderLabel");
 const rainbowButton = document.getElementById('rainbow');
 const clearButton = document.getElementById('clear');
+const modeButtons = document.querySelectorAll('.mode');
 
 let blockColor = colorPicker.value;
-let rainbow = false;
+let mode;
 
 function createGrid(n) {
+    //Create a n x n grid
     const grid = document.createElement('div')
     grid.classList.add('grid')
 
@@ -29,39 +31,62 @@ function createGrid(n) {
 }
 
 function getBlocks() {
+    //Add event listeners to all of the blocks
     const blocks = document.querySelectorAll('.block');
     blocks.forEach((block => {
         block.addEventListener('mouseover', () => {
-            if (!rainbow) {
+            if (mode === "regular") {
                 block.style.backgroundColor = blockColor;
             }
-            else {
+            else if (mode === "rainbow") {
                 block.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) +
                 ", " + Math.floor(Math.random() * 255) + ", " + 
-                Math.floor(Math.random() * 255) + ')'
+                Math.floor(Math.random() * 255) + ')';
             }
+            else {
+                
+                if (!block.style.backgroundColor) {
+                    block.shade = 200;
+                }
+                else if (block.shade === 0){
+                    return;
+                }
+                else {
+                    block.shade -= 10;
+                }
+                block.style.backgroundColor = 'rgb(' + block.shade + ', ' + block.shade + 
+                ', ' + block.shade + ')';
+
+            }
+            console.log(block.style.backgroundColor);
         })
+        console.log(block.style.backgroundColor);
     }))
 }
 
 function clear() {
+    //Clear the grid and create a new one
     let grid =  document.querySelector('.grid');
     grid.remove();
     createGrid(slider.value);
 }
 
+modeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.classList.add('clicked');
+        if (mode !== button.id) clear();
+        mode = button.id;
 
-rainbowButton.addEventListener('click', () => {
-    rainbow = !rainbow;
-    if (rainbow) {
-        rainbowButton.classList.add('pressed');
-        rainbowButton.style.backgroundColor = '#72317e';
-    }
-    else {
-        rainbowButton.classList.remove('pressed');
-        rainbowButton.style.backgroundColor = '';
-    }
+        modeButtons.forEach((button_other) => {
+            //Toggle the other buttons off
+            if (button_other !== button) {
+                button_other.classList.remove('clicked');
+            }
+            
+        })
+    })
 })
+
 
 colorPicker.addEventListener('change', () => {
     blockColor = colorPicker.value;
@@ -85,3 +110,4 @@ gridspace.style.marginRight = settings.offsetWidth+ 'px';
 
 createGrid(16);
 slider.value = 16;
+modeButtons[0].click() //Regular mode at start
