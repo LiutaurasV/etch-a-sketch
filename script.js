12d74eccc86a72a0cf3bd7e4acb567998101aebd
Liutauras <v.liutauras5@gmail.com>
2022-04-22 15:03:22 +0300

    Add settings

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const gridspace = document.querySelector('.gridspace');
+const settings = document.querySelector('.settings')
+const colorPicker = document.getElementById('colorPicker');
+const slider = document.querySelector('#slider');
+const sliderInfo = document.querySelector(".sliderLabel");
+const rainbowButton = document.getElementById('rainbow');
+const clearButton = document.getElementById('clear');
+
+let blockColor = colorPicker.value;
+let rainbow = false;
+
 function createGrid(n) {
     const grid = document.createElement('div')
     grid.classList.add('grid')
@@ -13,15 +24,63 @@ function createGrid(n) {
             line.appendChild(block);
         }
     }
-    document.body.appendChild(grid)
+    gridspace.appendChild(grid);
+    getBlocks();
 }
 
-createGrid(16);
+function getBlocks() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach((block => {
+        block.addEventListener('mouseover', () => {
+            if (!rainbow) {
+                block.style.backgroundColor = blockColor;
+            }
+            else {
+                block.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) +
+                ", " + Math.floor(Math.random() * 255) + ", " + 
+                Math.floor(Math.random() * 255) + ')'
+            }
+        })
+    }))
+}
 
-const blocks = document.querySelectorAll('.block');
-console.log(blocks);
-blocks.forEach((block => {
-    block.addEventListener('mouseover', (e) => {
-        block.style.backgroundColor = 'black'
-    })
-}))
+function clear() {
+    let grid =  document.querySelector('.grid');
+    grid.remove();
+    createGrid(slider.value);
+}
+
+
+rainbowButton.addEventListener('click', () => {
+    rainbow = !rainbow;
+    if (rainbow) {
+        rainbowButton.classList.add('pressed');
+        rainbowButton.style.backgroundColor = '#72317e';
+    }
+    else {
+        rainbowButton.classList.remove('pressed');
+        rainbowButton.style.backgroundColor = '';
+    }
+})
+
+colorPicker.addEventListener('change', () => {
+    blockColor = colorPicker.value;
+})
+
+slider.addEventListener('input', () => {
+    sliderInfo.textContent = "Grid size: " + slider.value + " x " + slider.value;
+})
+
+slider.addEventListener('change', () => {
+    clear();
+})
+
+clearButton.addEventListener('click', () => {
+    clear();
+})
+
+
+// Center the grid
+gridspace.style.marginRight = settings.offsetWidth+ 'px';
+
+createGrid(16);
